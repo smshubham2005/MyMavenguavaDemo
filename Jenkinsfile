3f844d2f8cbe66bd8f8361b529e122a11b61e6fa
shubhamhmath7@gmail.com <shubhamhmath7@gmail.com>
2026-06-18 22:03:50 +0530

    initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,41 @@
+pipeline {
+	agent any
+		tools{
+			mavem 'Maven'
+			}
+			
+			stages{
+			stage('Checkout'){
+				steps{
+					git branch: 'master' , url: 'https://github.com/smshubham2005/MyMavenGuavaDemo.git'
+					}
+				}
+				
+				stage('Build'){
+				steps{
+					sh ' mvn clean package'
+					}
+				}
+				
+				stage('Test'){
+				steps{
+					sh ' mvn test'
+					}
+				}
+				
+				stage('Run Application'){
+				steps{
+					sh ' mvn exec:java -Dexec.mainClass='com.example.App' '
+					}
+				}
+			}
+			
+			post{
+				success{
+					echo ' Build and Deployment successful'
+				}
+				failure{
+					echo ' Build failed '
+				}
+			}
+		}
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import com.google.common.collect.ImmutableList;
+
+public class App{
+	public static void main(String[] args){
+		ImmutableList<String> fruits = ImmutableList.of("Apple","Banana","Cherry");
+		System.out.println(fruits);
+		File sourceFile = new File("source.txt");
+		File destFile = new File("destination.txt");
+		
+		try{
+		FileUtils.copyFile(sourceFile,destFile);
+		System.out.println("File copied Successfully");
+		}
+		
+		catch(IOException e){
+			System.out.println("Error in Copying File"+e.getMessage());
+			}
+			
+			}
+			}
+			
+		
+
